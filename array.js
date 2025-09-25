b93690e16cf9be592d25e9288be5afb39c3321c9
leoBNDR-BR <leonardobender94@gmail.com>
2025-09-25 13:45:12 -0300

    commit 1

---
+++ array.js
@@ -0,0 +1,8 @@
+var times = ['São Paulo', 'Corinthians', 'Palmeiras', 'Santos'];
+
+console.log(times.length); // 4
+console.log(times[0]); // São Paulo
+console.table(times);
+times[1] = 'Grêmio';
+
+console.table(times);
---
+++ arrayMultidimensional.js
@@ -0,0 +1,4 @@
+var boletim = [[8,7,9,3],[4,5,8,6]];
+
+console.log(boletim);
+console.table(boletim);
